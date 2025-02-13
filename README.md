3ce5a498b0d031d445d3dba4042ecc56e98b2d56
parv <parviz.shovkatov21@outlook.com>
2025-02-13 02:59:26 +0000

    added a fucntioning data base

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # GroupProject-WebMarket
 Our group project repository
 Using tailwind css
+temporary message for group: to use data base you mght have to dowload my sql on your device along wiht node.js
--- src/db.js
+++ src/db.js
@@ -1,15 +1,13 @@
-const mysql = require('mysql');
+import mysql from 'mysql2'
 
-// Create a connection to the database
-const con = mysql.createConnection({
-  host: 'localhost',   // Fixed the space here
-  user: 'root',
-  password: 'Brighton123',        // Make sure to set the correct password if needed
-  database: 'mydb'     // Ensure this is the name of your actual database
-});
+const mydb = 'mydb';
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Brighton123',
+    database: mydb
 
-// Connect to the database
-con.connect(function(err) {
-  if (err) throw err;  // If there's an error, throw it
-  console.log('Connected!');  // Successfully connected
-});
+}) .promise();
+
+const result = await pool.query("SELECT * FROM users")
+console.log(result);
