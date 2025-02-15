9693c5742500d7811ec0ce88d8f3e3d97e47a289
parv <parviz.shovkatov21@outlook.com>
2025-02-15 00:53:00 +0000

    added ablity to add data to databse

--- src/db.js
+++ src/db.js
@@ -1,4 +1,9 @@
 import mysql from 'mysql2'
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
 
 const mydb = 'mydb';
 const pool = mysql.createPool({
@@ -11,3 +16,14 @@ const pool = mysql.createPool({
 
 const result = await pool.query("SELECT * FROM users")
 console.log(result);
+
+async function createUser (userID, firstname, secondname, username, email, phoneNumber){ // what rows are affceted by insert statment 
+const result = await pool.query("INSERT INTO users (userID, firstname, secondname, username, email, phoneNumber) VALUES (?,?,?,?,?,?)", // sql querey to insert data into data base 
+[userID, firstname, secondname, username, email, phoneNumber]);
+
+return result; // returns 
+
+} 
+
+const testdata = await createUser("0002", "big", "john", "bigjohn123", "bigjohn2@outlook.com", "07452133453");
+console.log(testdata);
